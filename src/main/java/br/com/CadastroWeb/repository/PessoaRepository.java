aa34b99191875193f4e5457492dffb3598575c8c
Alexandre Eustaquio <alexeus.souza@gmail.com>
2025-04-27 22:47:24 -0300

    feat: criar repositório JPA para Pessoa

---
+++ src/main/java/br/com/CadastroWeb/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package br.com.CadastroWeb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.CadastroWeb.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
