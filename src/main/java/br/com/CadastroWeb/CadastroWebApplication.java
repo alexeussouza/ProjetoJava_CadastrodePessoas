3801584e78bbedf722eb7a25bdc8181d1fd9ccbc
Alexandre Eustaquio <alexeus.souza@gmail.com>
2025-04-27 22:40:48 -0300

    feat: criação das branchs main e develop

---
+++ src/main/java/br/com/CadastroWeb/CadastroWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.CadastroWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/CadastroWeb/CadastroWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.CadastroWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
